796afda3908250bfdf308c2b78c23c9d47171517
nairx <nairxprv@gmail.com>
2026-09-21 13:38:18 +0530

    updated

---
+++ P02_ConditionalStatements/p1.java
@@ -0,0 +1,13 @@
+package P02_ConditionalStatements;
+
+public class p1 {
+    public static void main(String[] args) {
+        int age = 20;
+        if (age>18){
+            System.out.println("Adult");
+        }
+        else {
+            System.out.println("Minor");
+        }
+    }
+}
---
+++ P02_ConditionalStatements/p2.java
@@ -0,0 +1,16 @@
+package P02_ConditionalStatements;
+
+public class p2 {
+    public static void main(String[] args) {
+        int marks = 75;
+        if (marks >= 90){
+            System.out.println("A");
+        }
+        else if (marks >= 75){
+            System.out.println("B");
+        }
+        else{
+            System.out.println("C");
+        }
+    }
+}
